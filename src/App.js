681f03007a6cef5a438f5531d99e122c32512473
pacod8 <pacodominguezg@gmail.com>
2022-09-05 16:55:13 +0200

    animation for icons

--- src/App.js
+++ src/App.js
@@ -13,41 +13,41 @@ function App() {
   <div className='container'>
   </div>
   <div className='info scale-up-ver-center'>
-    <div class="youtube">
-      <a href='https://www.youtube.com' target="_blank"><CircumIcon name="youtube" color="white" size="60px"/></a>
+    <div className="youtube">
+      <a href='https://www.youtube.com' className='icon' target="_blank" rel="noreferrer"><CircumIcon name="youtube" color="white" size="60px"/></a>
     </div>
-    <div class="github">
-      <a href='https://github.com/pacod8' target="_blank"><CircumIcon name="route" color="white" size="60px"/></a>
+    <div className="github">
+      <a href='https://github.com/pacod8' className='icon' target="_blank" rel="noreferrer"><CircumIcon name="route" color="white" size="60px"/></a>
     </div>
-    <div class="notion">
-      <a href='https://www.notion.so' target="_blank"><CircumIcon name="bacon" color="white" size="60px"/></a>
+    <div className="notion">
+      <a href='https://www.notion.so' className='icon' target="_blank" rel="noreferrer"><CircumIcon name="bacon" color="white" size="60px"/></a>
     </div>
-    <div class="linkedin">
-      <a href='https://www.linkedin.com' target="_blank"><CircumIcon name="linkedin" color="white" size="60px"/></a>
+    <div className="linkedin">
+      <a href='https://www.linkedin.com' className='icon' target="_blank" rel="noreferrer"><CircumIcon name="linkedin" color="white" size="60px"/></a>
     </div>
-    <div class="whatsapp">
-      <a href='https://web.whatsapp.com' target="_blank"><CircumIcon name="chat_1" color="white" size="60px"/></a>
+    <div className="whatsapp">
+      <a href='https://web.whatsapp.com' className='icon' target="_blank" rel="noreferrer"><CircumIcon name="chat_1" color="white" size="60px"/></a>
     </div>
-    <div class="calendar">
-      <a href='https://calendar.google.com' target="_blank"><CircumIcon name="calendar" color="white" size="60px"/></a>
+    <div className="calendar">
+      <a href='https://calendar.google.com' className='icon' target="_blank" rel="noreferrer"><CircumIcon name="calendar" color="white" size="60px"/></a>
     </div>
-    <div class="gmail">
-      <a href='https://mail.google.com' target="_blank"><CircumIcon name="mail" color="white" size="60px"/></a>
+    <div className="gmail">
+      <a href='https://mail.google.com' className='icon' target="_blank" rel="noreferrer"><CircumIcon name="mail" color="white" size="60px"/></a>
     </div>
-    <div class="tidal">
-      <a href='https://listen.tidal.com/' target="_blank"><CircumIcon name="music_note_1" color="white" size="60px"/></a>
+    <div className="tidal">
+      <a href='https://listen.tidal.com/' className='icon' target="_blank" rel="noreferrer"><CircumIcon name="music_note_1" color="white" size="60px"/></a>
     </div>
-    <div class="onedrive">
-      <a href='https://liveutad-my.sharepoint.com' target="_blank"><CircumIcon name="cloud" color="white" size="60px"/></a>
+    <div className="onedrive">
+      <a href='https://liveutad-my.sharepoint.com' className='icon' target="_blank" rel="noreferrer"><CircumIcon name="cloud" color="white" size="60px"/></a>
     </div>
-    <div class="matter">
-      <a href='https://web.getmatter.app' target="_blank"><CircumIcon name="bookmark" color="white" size="60px"/></a>
+    <div className="matter">
+      <a href='https://web.getmatter.app' className='icon' target="_blank" rel="noreferrer"><CircumIcon name="bookmark" color="white" size="60px"/></a>
     </div>
-    <div class="twitter">
-      <a href='https://twitter.com/home' target="_blank"><CircumIcon name="twitter" color="white" size="60px"/></a>
+    <div className="twitter">
+      <a href='https://twitter.com/home' className='icon' target="_blank" rel="noreferrer"><CircumIcon name="twitter" color="white" size="60px"/></a>
     </div>
-    <div class="todoist">
-      <a href='https://todoist.com/app/today' target="_blank"><CircumIcon name="circle_list" color="white" size="60px"/></a>
+    <div className="todoist">
+      <a href='https://todoist.com/app/today' className='icon' target="_blank" rel="noreferrer"><CircumIcon name="circle_list" color="white" size="60px"/></a>
     </div>
   </div>
 </div>
